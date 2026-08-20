74191a3a4ef7260e7dd3a46252e702eb03d88f02
Sanjith-Gunasekaran <sanjith1294@gmail.com>
2026-08-21 02:10:37 +0800

    Completed Level 1. Added functionality to echo whatever user inputs unless its "bye" in which case the bot ends the conversation.

--- src/main/java/Heisenberg.java
+++ src/main/java/Heisenberg.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Heisenberg {
     public static void main(String[] args) {
         String heisenberg = """
@@ -20,9 +22,16 @@ public class Heisenberg {
                 My name is Walter Hartwell White. 
                 I live at 308 Negra Arroyo Lane, Albuquerque, New Mexico, 87104. 
                 What can I do for you?
-                Bye!
              """;
 
         System.out.print(heisenberg);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+        System.out.print("Goodbye!");
     }
 }
