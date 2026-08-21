05c483622b9ae422f7787915c776166b31877649
Sanjith-Gunasekaran <sanjith1294@gmail.com>
2026-08-21 16:48:29 +0800

    Level 3 completed. Added functionality to mark items in the list and edited the UI by changing the print statements.

--- src/main/java/Heisenberg.java
+++ src/main/java/Heisenberg.java
@@ -27,15 +27,23 @@ public class Heisenberg {
              """;
 
         System.out.print(heisenberg);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String input;
         boolean running = true;
         while(running) {
             input = scanner.nextLine();
+            String[] parts = input.trim().split("\\s+");
+
+            if(checkMark(parts)) {
+                mark(Integer.parseInt(parts[1]), list);
+                continue;
+            }
+
             switch(input) {
                 case "list":
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < list.size(); i++) {
                         System.out.printf("%d. %s%n", i + 1, list.get(i));
                     }
@@ -45,9 +53,25 @@ public class Heisenberg {
                     running = false;
                     break;
                 default:
-                    list.add(input);
+                    list.add(new Task(input));
                     System.out.printf("added: %s%n", input);
             }
         }
+    }
+    private static boolean checkMark(String[] parts) {
+        if(parts.length == 2) {
+            try {
+                Integer.parseInt(parts[1]);
+                return parts[0].equals("mark");
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private static void mark(int index, List<Task> list) {
+        list.get(index - 1).mark();
+        System.out.println("Nice! I've marked this task as done: \n" + list.get(index - 1));
     }
 }
