de077772b74e8aa3806b08683cdc293d733087ec
Sanjith-Gunasekaran <sanjith1294@gmail.com>
2026-08-21 13:35:25 +0800

    Level 2 completed. Added functionality to add items to list and print list.

--- src/main/java/Heisenberg.java
+++ src/main/java/Heisenberg.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Heisenberg {
     public static void main(String[] args) {
@@ -25,13 +27,27 @@ public class Heisenberg {
              """;
 
         System.out.print(heisenberg);
+        List<String> list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while(!input.equals("bye")) {
-            System.out.println(input);
+        String input;
+        boolean running = true;
+        while(running) {
             input = scanner.nextLine();
+            switch(input) {
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.printf("%d. %s%n", i + 1, list.get(i));
+                    }
+                    break;
+                case "bye":
+                    System.out.print("Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    list.add(input);
+                    System.out.printf("added: %s%n", input);
+            }
         }
-        System.out.print("Goodbye!");
     }
 }
